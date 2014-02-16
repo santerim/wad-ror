cfa244379a2cabf87735ab9df1a35f2dab73baab
Santeri Martikainen <santerim@cs.helsinki.fi>
2014-02-16 23:55:11 +0200

    sunnuntain vika

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,20 +3,10 @@ class PlacesController < ApplicationController
 	end
 
 	def search
-		api_key = "6dc19e84f1c1fea78cc24963dcb85b41"
-		url = "http://beermapping.com/webservice/loccity/#{api_key}/"
-
-		response = HTTParty.get "#{url}#{ERB::Util.url_encode(params[:city])}"
-		places = response.parsed_response["bmp_locations"]["location"]
-		
-		if places.is_a?(Hash) and places['id'].nil?
-			redirect_to places_path, :notice => "No places in #{params[:city]}"
+		@places = BeermappingApi.places_in(params[:city])
+		if @places.empty?
+			redirect_to places_path, :notice => "No locations in #{params[:city]}"
 		else
-			places = [places] if places.is_a?(Hash)
-			@places = places.inject([]) do |set, location|
-				set << Place.new(location)
-			end
-
 			render :index
 		end
 	end
